6f15a00b110b59c8f318ecde1348c2185e03b4f8
JSWuMing <57783981+JSWuMing@users.noreply.github.com>
2023-06-27 00:40:28 +0800

    让这个插件更适合我

--- src/main/kotlin/org/fastmcmirror/ultrapay/commands/PayCommand.kt
+++ src/main/kotlin/org/fastmcmirror/ultrapay/commands/PayCommand.kt
@@ -6,7 +6,6 @@ import org.bukkit.entity.Player
 import org.fastmcmirror.ultrapay.UltraPay
 import org.fastmcmirror.ultrapay.api.PayAPI
 import org.fastmcmirror.ultrapay.api.PaymentType
-import org.fastmcmirror.ultrapay.gui.PayGUIHolder
 import org.fastmcmirror.ultrapay.utils.Color.color
 import org.fastmcmirror.ultrapay.utils.RewardParser
 import taboolib.common.platform.command.CommandBody
@@ -23,79 +22,55 @@ import taboolib.common.platform.command.mainCommand
 object PayCommand {
     @CommandBody
     var main = mainCommand {
-        literal("open") {
-            dynamic {
-                execute<Player> { sender, _, argument ->
-                    run {
-                        if (!argument.matches("-?\\d+(\\.\\d+)?".toRegex())) {
-                            sender.sendMessage("&c&l参数无效: 请传入一个数字!".color())
-                            return@run
-                        }
-                        if (argument.toDouble() < 0.01) {
-                            sender.sendMessage("&c&l参数无效: 传入的数字过小!".color())
-                            return@run
-                        }
-                        val holder = PayGUIHolder(argument.toDouble())
-                        sender.openInventory(holder.inventory)
-                    }
-                }
-            }
-        }
         literal("pay") {
-            literal("wx") {
-                dynamic {
-                    execute<Player> { sender, _, argument ->
-                        run {
-                            if (!argument.matches("-?\\d+(\\.\\d+)?".toRegex())) {
-                                sender.sendMessage("&c&l参数无效: 请传入一个数字!".color())
-                                return@run
-                            }
-                            if (argument.toDouble() < 0.01) {
-                                sender.sendMessage("&c&l参数无效: 传入的数字过小!".color())
-                                return@run
-                            }
-                            val price = argument.toDouble()
-                            val future = PayAPI.createOrder(
-                                PaymentType.WECHAT_PAY,
-                                UltraPay.bukkitInstance.config.getString("itemName")!!
-                                    .replace("%price%", price.toString()),
-                                price,
-                                sender
-                            )
-                            future.thenAcceptAsync { order ->
-                                RewardParser.parseReward(UltraPay.bukkitInstance.config.getStringList("reward"), order)
-                            }
-                        }
-                    }
-                }
-            }
             literal("zfb") {
-                dynamic {
-                    execute<Player> { sender, _, argument ->
-                        run {
-                            if (!argument.matches("-?\\d+(\\.\\d+)?".toRegex())) {
-                                sender.sendMessage("&c&l参数无效: 请传入一个数字!".color())
-                                return@run
-                            }
-                            if (argument.toDouble() < 0.01) {
-                                sender.sendMessage("&c&l参数无效: 传入的数字过小!".color())
-                                return@run
-                            }
-                            val price = argument.toDouble()
-                            val future = PayAPI.createOrder(
-                                PaymentType.ALIPAY,
-                                UltraPay.bukkitInstance.config.getString("itemName")!!
-                                    .replace("%price%", price.toString()),
-                                price,
-                                sender
-                            )
-                            future.thenAcceptAsync { order ->
-                                RewardParser.parseReward(UltraPay.bukkitInstance.config.getStringList("reward"), order)
+                literal("point"){
+                    dynamic {
+                        execute<Player> { sender, _, argument ->
+                            run {
+                                if (!argument.matches("-?\\d+(\\.\\d+)?".toRegex())) {
+                                    sender.sendMessage("&c&l参数无效: 请传入一个数字!".color())
+                                    return@run
+                                }
+                                if (argument.toDouble() < 0.01) {
+                                    sender.sendMessage("&c&l参数无效: 传入的数字过小!".color())
+                                    return@run
+                                }
+                                val price = argument.toDouble()
+                                val future = PayAPI.createOrder(
+                                        PaymentType.ALIPAY,
+                                        UltraPay.bukkitInstance.config.getString("type.points.itemName")!!
+                                                .replace("%price%", price.toString()),
+                                        price,
+                                        sender
+                                )
+                                future.thenAcceptAsync { order ->
+                                    RewardParser.parseReward(UltraPay.bukkitInstance.config.getStringList("type.points.reward"), order)
+                                }
                             }
                         }
                     }
                 }
-            }
+                literal("item"){
+                    dynamic {
+                        execute<Player> { sender, _, argument ->
+                            run {
+
+                                val price = UltraPay.bukkitInstance.config.getDouble("type.items.${argument}.price")
+                                val future = PayAPI.createOrder(
+                                        PaymentType.ALIPAY,
+                                        UltraPay.bukkitInstance.config.getString("type.items.${argument}")!!
+                                                .replace("%price%", price.toString()),
+                                        price,
+                                        sender
+                                )
+                                future.thenAcceptAsync { order ->
+                                    RewardParser.parseReward(UltraPay.bukkitInstance.config.getStringList("type.items.${argument}.reward"), order)
+                                }
+                            }
+                        }
+                    }
+                }
         }
         literal("reload", permission = "ultrapay.admin") {
             execute<CommandSender> { sender, _, _ ->
@@ -110,12 +85,10 @@ object PayCommand {
         execute<CommandSender> { sender, context, _ ->
             run {
                 sender.sendMessage(("&b&lUltraPay &7- &ev" + UltraPay.bukkitInstance.description.version).color())
-                sender.sendMessage("&c/ultrapay open <金额> &7———— &e打开支付方式选择界面".color())
-                sender.sendMessage("&c/ultrapay pay <付款方式: wx/zfb> <金额> &7———— &e获取支付二维码".color())
                 if (sender.hasPermission("ultrapay.admin")) {
                     sender.sendMessage("&c/ultrapay reload &7———— &e重载插件".color())
                 }
             }
         }
     }
-}
+}}
